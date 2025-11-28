e2f5246b214e138fc3946286d3fc70924f996fab
YuriDamin <yuribonfant@gmail.comgit config --global user.name YuriDamingit config --get-all user.namegit config --global user.email yuribonfant@gmail.com>
2025-11-27 23:23:48 -0300

    teste2ww
    teste

---
+++ front/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+
+  const protectedRoutes = ["/board"];
+
+  const tryingToAccessProtectedRoute = protectedRoutes.some((route) =>
+    req.nextUrl.pathname.startsWith(route)
+  );
+
+  if (tryingToAccessProtectedRoute && !token) {
+    
+    const loginUrl = new URL("/login", req.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/board/:path*", "/board"],
+};
